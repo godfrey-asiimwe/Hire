accd75cdf8035323b5335b8801ec0b589d99dac1
godfrey-asiimwe <agtumusiime@gmail.com>
2022-03-22 08:29:02 +0300

    22nd March 2022  updates

--- core/rec/recuit/admin.py
+++ core/rec/recuit/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 # Register your models here.
 # Register your models here.
 
-from .models import JobType, Job, salaryScale, YearOfExp, EducationLevel
+from .models import JobType, Job, salaryScale, YearOfExp, EducationLevel,UserProfile
 
 
 class jobType(admin.ModelAdmin):
